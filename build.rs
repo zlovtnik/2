11e18549b4ac6b5c0db5835688503c4f05a7313f
RCS <github-actions[bot]@users.noreply.github.com>
2025-07-23 14:32:31 -0400

    Generate `user_stats.bin` from Protobuf definitions during build process.

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
-fn main() {
-    // This will help us see the expanded macros
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src/**/*.rs");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    tonic_build::configure()
+        .file_descriptor_set_path(format!("{}/user_stats.bin", out_dir))
+        .compile(&["proto/user_stats.proto"], &["proto"])?;
+    Ok(())
 }
